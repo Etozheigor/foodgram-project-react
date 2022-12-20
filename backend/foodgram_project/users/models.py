d6769120da9f9de8660f27923269e7ab8ac60bc5
Игорь Ищенко <itshenko.io@gmail.com>
2022-12-20 21:03:04 +0300

    abstract user model added

--- backend/foodgram_project/users/models.py
+++ backend/foodgram_project/users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+USER = 'user'
+ADMIN = 'admin'
+ROLE_CHOICES = [(USER, 'user'), (ADMIN, 'admin')]
+
+class User(AbstractUser):
+    email = models.EmailField(verbose_name='email address', blank=True, max_length=254)
+    first_name = models.CharField(verbose_name='имя', blank=True, max_length=150)
+    last_name = models.CharField(verbose_name='фамилия', blank=True, max_length=150)
+    username = models.CharField(verbose_name='username', blank=True, max_length=150, unique=True)
+    password = models.CharField(verbose_name='пароль', blank=True, max_length=150)
+    role = models.CharField(verbose_name='роль', default=USER, choices=ROLE_CHOICES)
+
