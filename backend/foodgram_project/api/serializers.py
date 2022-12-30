5ecc70470480633dfb84623ec226884e7535c379
Игорь Ищенко <itshenko.io@gmail.com>
2022-12-30 18:03:39 +0300

    full users api

--- backend/foodgram_project/api/serializers.py
+++ backend/foodgram_project/api/serializers.py
@@ -1,6 +1,28 @@
 from rest_framework import serializers
 from users.models import User
 from recipes.models import Ingredient, Tag, Recipe, RecipeTag, RecipeIngredientAmount, ShoppingCart, Favorite, Follow
+from rest_framework.validators import UniqueValidator
+
+class UserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(max_length=254, validators=(UniqueValidator(queryset=User.objects.all(), message="Данный email уже сущестует"),))
+    is_subscribed = serializers.SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = ('email', 'id',  'username', 'first_name', 'last_name', 'is_subscribed')
+
+
+    def get_is_subscribed(self, obj):
+        return Follow.objects.filter(follower=self.context['request'].user, following=obj).exists()
+
+
+class UserCreateSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(max_length=254, validators=(UniqueValidator(queryset=User.objects.all(), message="Данный email уже сущестует"),))
+
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'username', 'last_name', 'first_name')
+
 
 class TagSerializer(serializers.ModelSerializer):
 
--- backend/foodgram_project/foodgram_project/settings.py
+++ backend/foodgram_project/foodgram_project/settings.py
@@ -134,14 +134,26 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated', 
+        'rest_framework.permissions.AllowAny', 
     ],
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
+
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5,
 }
 
+DJOSER = {
+    'SERIALIZERS' : {
+        'user' : 'api.serializers.UserSerializer',
+        'user_create': 'api.serializers.UserCreateSerializer',
+        'current_user': 'api.serializers.UserSerializer' },
+    'LOGIN_FIELD': 'email',
+
+
+}
 
 # SIMPLE_JWT = {
 #    'ACCESS_TOKEN_LIFETIME': timedelta(days=30),
